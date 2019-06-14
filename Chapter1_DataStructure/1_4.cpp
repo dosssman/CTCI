30cf191034e181bccfa7aa77c22077c6d2fde776
Dossa Rousslan Fernand Julien <dosssman@hotmail.fr>
2019-06-14 17:19:56 +0900

    Solved 1_4, working on 1_5

--- Chapter1_DataStructure/1_4.cpp
+++ Chapter1_DataStructure/1_4.cpp
@@ -3,7 +3,35 @@
 #include <string>
 using namespace std;
 
+#define MAX_SIZE 18
+
+void shift2Right( char *, const int );
+
 int main() {
+	char str[MAX_SIZE] = "Mr John Smith\0   ";
+
+	cout << "Before: " << str << endl;
+
+	for( int i = 0; str[i] != '\0'; i++) {
+		if( str[i] == ' ') {
+			shift2Right( str, i);
+			*(str+i) = '%';
+			*(str+(i+1)) = '2';
+			*(str+(i+2)) = '0';
+			
+			i+=2;
+		}
+	}
+
+	cout << str << endl;
 
 	return 0;
 }
+
+void shift2Right( char *str, const int currentIdx) {
+	int lastIdx = MAX_SIZE-1;
+	while( lastIdx > currentIdx+2) {
+		str[lastIdx] = str[lastIdx-2];
+		lastIdx--;
+	}
+};
---
+++ Chapter1_DataStructure/1_5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+char *compStr( char *) ;
+
+#define MAX_SIZE 255;
+
+int main() {
+
+
+    return 0;
+}
+
+char *compStr( char *str) {
+
+    return str;
+}
