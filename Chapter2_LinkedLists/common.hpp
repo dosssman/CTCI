c6aa3be04a2626b09e5759c8d136c959e21d6552
z3r0 <dosssman@hotmail.fr>
2019-06-20 11:11:43 +0900

    Added 1-8 and Chapter2 Dir

---
+++ Chapter2_LinkedLists/common.hpp
@@ -0,0 +1,46 @@
+class LinkedNode {
+private:
+  LinkedNode *next;
+  int *value;
+public:
+  LinkedNode *getNext() {
+    return next;
+  }
+  void setNext( LinkedNode *node) {
+    if( next != NULL) {
+      free( next);
+      next = NULL;
+    }
+    next = node;
+  }
+  int getValue() {
+    return *value;
+  }
+  int *setValue( const int v) {
+    if( value != NULL) {
+      free( value);
+      value = NULL;
+    }
+    value = new int( v);
+
+    return value;
+  }
+
+LinkedNode() {
+  next = NULL;
+  value = NULL;
+}
+LinkedNode( const int v) {
+  this.value = new int( v);
+  this.next = NULL;
+}
+LinkedNode( const int v, const LinkedNode *next) {
+  this.value = new int( v);
+  this.next = next;
+}
+LinkedNode()~ {
+  free( next);
+  free( value);
+}
+
+}
