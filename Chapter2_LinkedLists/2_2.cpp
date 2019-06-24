05118aa6528cb82c2e106c38ec411aeb4cb5851f
Dossa Rousslan Fernand Julien <dosssman@hotmail.fr>
2019-06-24 12:52:10 +0900

    Solved 2_2

--- Chapter2_LinkedLists/2_2.cpp
+++ Chapter2_LinkedLists/2_2.cpp
@@ -2,12 +2,40 @@
 #include "common.hpp"
 using namespace std;
 
+LinkedNode *getKthElementFromEnd( LinkedNode *head, const int &k);
+
 int main() {
 	auto head = new LinkedNode( 0);
 
-	for( auto i=1; i<5; i++) {
+	for( auto i=1; i<20; i++) {
 		head->appendToTail( i);
 	}
 
+	displayLinkedList( head);
+	cout << "3 from end " << getKthElementFromEnd( head, 3) << endl;
+
 	return 0;
+}
+
+LinkedNode *getKthElementFromEnd( LinkedNode *head, const int &k) {
+	if( head == NULL) return NULL;
+
+	int dist = 0;
+
+	LinkedNode *frontP = head, *backP = head;
+
+	while( frontP != NULL) {
+		frontP = frontP->getNext();
+		if( dist == k) {
+			backP = backP->getNext();
+		} else if( dist < k) {
+			dist++;
+		}
+	}
+
+	if( dist == k) {
+		return backP;
+	} else {
+		return NULL;
+	}
 }
